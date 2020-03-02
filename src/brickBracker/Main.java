f4d06c6ce946db5257552d4c687f8e0e6dd04faf
jiten001 <61696392+jiten001@users.noreply.github.com>
2020-03-02 19:52:39 +0545

    init commit

---
+++ src/brickBracker/Main.java
@@ -0,0 +1,17 @@
+package brickBracker;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args){
+        JFrame obj = new JFrame();
+        GamePlay gamePlay = new GamePlay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("BrickGame");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+    }
+}
